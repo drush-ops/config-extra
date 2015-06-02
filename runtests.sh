599fe8a25a28dc7d760b914016c13a2ada412f72
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-06-02 10:40:51 -0700

    Adjust phpunit configuration for travis.

--- runtests.sh
+++ runtests.sh
@@ -8,21 +8,26 @@
 # Any parameters that may be passed to phpunit may also be used
 # with runtests.sh.
 
+# We use the Drush that is on the global $PATH. See .travis.yml
 DRUSH_PATH="$(which drush)"
 DRUSH_DIR="$(dirname -- "$DRUSH_PATH")"
 
+# Prefer the phpunit that Drush bundles, if available
 PHPUNIT=phpunit
-
 if [ -f "$DRUSH_DIR/vendor/bin/phpunit" ]
 then
   PHPUNIT="$DRUSH_DIR/vendor/bin/phpunit"
 fi
 
-echo "DRUSH_PATH is $DRUSH_PATH"
-echo "PHPUNIT is $PHPUNIT"
+# Prefer testing with Drupal 8 (maybe Drupal 7 will work someday, but not today)
+if [ -z "$UNISH_DRUPAL_MAJOR_VERSION" ]
+then
+  export UNISH_DRUPAL_MAJOR_VERSION=8
+fi
 
+# Run all tests if there were no arguments
 if [ $# = 0 ] ; then
-  UNISH_DRUPAL_MAJOR_VERSION=8 UNISH_NO_TIMEOUTS=y $PHPUNIT --bootstrap="$DRUSH_DIR/tests/bootstrap.inc" tests
+  UNISH_NO_TIMEOUTS=y $PHPUNIT --bootstrap="$DRUSH_DIR/tests/bootstrap.inc" tests
 else
-  UNISH_DRUPAL_MAJOR_VERSION=8 UNISH_NO_TIMEOUTS=y $PHPUNIT --bootstrap="$DRUSH_DIR/tests/bootstrap.inc" "$@"
+  UNISH_NO_TIMEOUTS=y $PHPUNIT --bootstrap="$DRUSH_DIR/tests/bootstrap.inc" "$@"
 fi
